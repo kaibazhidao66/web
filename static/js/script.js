b3e4c6f6ea65dacef11c1f5fc887bbe4dea2865c
xukai <3353212714@qq.com>
2022-04-02 12:27:59 +0800

    gx

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,3 @@
-/* 禁止右键，F12 */
-function unmouse(){
-	document.oncontextmenu = new Function("return false;");
-	document.onkeydown = document.onkeyup = document.onkeypress = function(event) {
-		var e = event || window.event || arguments.callee.caller.arguments[0];
-		if (e && (e.keyCode == 123 || (e.keyCode == 116 && e.type!='keypress')))
-		{
-			e.returnValue = false;
-			return (false);
-		}
-	}
-}
-unmouse()
-
 //控制台输出
 var styleTitle1 = `
 font-size: 20px;
